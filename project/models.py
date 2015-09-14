58abb2f30aec35f2a2db581d41e2ab937f0142c3
tiny-rejoan <rejoanul.alam@gmail.com>
2015-09-14 18:04:37 +0600

    model commit test

--- project/models.py
+++ project/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    create_date = models.DateTimeField('date created')
+    deadline = models.DateTimeField('deadline')
+    status = models.CharField(max_length=100)
+
+class Milestone(models.Model):
+    project_id = models.ForeignKey(Project)
+    title = models.CharField(max_length=200)
+    descriptions = models.TextField()
+    start_date = models.DateTimeField('start date')
