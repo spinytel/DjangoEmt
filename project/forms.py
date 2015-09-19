8fd01828248bc14f7f411b7e0f9741925ae36679
tiny-rejoan <rejoanul.alam@gmail.com>
2015-09-19 11:39:48 +0600

    Forms

--- project/forms.py
+++ project/forms.py
@@ -9,10 +9,6 @@ class ProjectForm(forms.Form):
     deadline = forms.DateTimeField(label='Deadline', widget=forms.TextInput(attrs={'class':'datetime_picker','placeholder':'Estimated Project Deadline','autocomplete':'off'}))
     project_file = forms.FileField(label='Project Files')
 
-class ProjectEditForm(forms.Form):
-    name = forms.CharField(label='Project Name', max_length=200, required=True, widget=forms.TextInput(attrs={'placeholder':'Project Name'}))
-    deadline = forms.DateTimeField(label='Deadline', widget=forms.TextInput(attrs={'class':'datetime_picker','placeholder':'Estimated Project Deadline','autocomplete':'off'}))
-    project_file = forms.FileField(label='Project Files')
 
 class MilestoneForm(forms.Form):
     def __init__(self,*args,**kwargs):
