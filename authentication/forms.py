4e293d4a21ab168edc757a5058e58bf388f8cf77
mamun0024 <nirjhor.boss@gmail.com>
2015-09-30 17:07:05 +0600

    Update Signature

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from authentication.models import Account
 
 
+# Author : @mamun0024
 class UserForm(forms.ModelForm):
     username = forms.CharField(label='Name', max_length=200, required=True, widget=forms.TextInput(attrs={'placeholder': 'User Name'}))
     email = forms.CharField(label='Email', required=True, widget=forms.TextInput(attrs={'placeholder': 'User Email'}))
@@ -21,6 +22,7 @@ class UserForm(forms.ModelForm):
         return user
 
 
+# Author : @mamun0024
 class UserEditForm(forms.Form):
     username = forms.CharField(label='Name', max_length=200, required=True, widget=forms.TextInput(attrs={'placeholder': 'User Name'}))
     email = forms.CharField(label='Email', required=True, widget=forms.TextInput(attrs={'placeholder': 'User Email'}))
